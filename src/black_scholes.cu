14499d0f4845b7920e15ae63f337d32c5f6b0ce4
Changok Kim <ck1334@cuda4.cims.nyu.edu>
2012-04-25 14:48:35 -0400

    almost done gaussian

--- src/black_scholes.cu
+++ src/black_scholes.cu
@@ -30,15 +30,15 @@ __device__ double black_scholes_value (const double S,
       ((current_value - E < 0.0) ? 0.0 : current_value - E);
 }
 
-__device__ gaussrand_result_t gaussrand (const double u1, const double u2) {
+__device__ gaussrand_result_t gaussrand (curandState* localState) {
   gaussrand_result_t result;
 
   //if (u1==u2) return -1.0;
 
   double v1, v2, s;
   do {
-    v1 = 2.0 * u1 - 1;
-    v2 = 2.0 * u2 - 1;
+    v1 = 2.0 * curand_uniform(localState) - 1.0;
+    v2 = 2.0 * curand_uniform(localState) - 1.0;
     s = v1 * v1 + v2 * v2;
   } while (s >= 1 || s== 0);
 
@@ -55,8 +55,11 @@ __global__ void black_scholes_kernel(const double S, const double E,
             curandState* randStates, double* debug) {
     
     __shared__ double sum_of_trials[WINDOW_WIDTH];
-    
-    unsigned int gId = (blockIdx.x * WINDOW_WIDTH) + threadIdx.x; 
+   
+    /* Since we decreased ACTUAL size of block to the half
+	   , gId which is thread position in KERNEL will be jumped
+	   by WINDOW_WIDTH/2 at each end of tId in a block */
+    unsigned int gId = (blockIdx.x * WINDOW_WIDTH) + (threadIdx.x); 
     unsigned int tId = threadIdx.x;
 
     // Do the Black-Scholes iterations
@@ -65,20 +68,30 @@ __global__ void black_scholes_kernel(const double S, const double E,
     double RANDOM = curand_uniform( &localState );
     //double unirand1 = curand_uniform( &localState );
     //double unirand2 = curand_uniform( &localState );
-    //gaussrand_result_t gresult = guassrand (curand_uniform (&localState), curand_uniform (&localState));
+    gaussrand_result_t gresult = gaussrand (&localState);
 // prng test
 //debug[gId] = grand1;
-debug[gId] = RANDOM;
+debug[gId] = gresult.grand1;
+//debug[gId+(WINDOW_WIDTH/2)] = gresult.grand2;
 // prng test end
     randStates[gId] = localState;
 
-    double value = black_scholes_value (S, E, r, sigma, T, RANDOM);
-    cudaTrials[gId] = value;
+    //double value = black_scholes_value (S, E, r, sigma, T, RANDOM);
+    //cudaTrials[gId] = value;
+    gresult.grand1 = black_scholes_value (S, E, r, sigma, T, gresult.grand1);
+    cudaTrials[gId] = gresult.grand1;
+//    gresult.grand2 = black_scholes_value (S, E, r, sigma, T, gresult.grand2);
+    //cudaTrials[gId+(WINDOW_WIDTH/2)] = gresult.grand2;
     
     // we need to keep origianl trial values for calculatng standard deviation
-    sum_of_trials[tId] = value;
+    //sum_of_trials[tId] = value;
+    sum_of_trials[tId] = gresult.grand1;
+//    sum_of_trials[tId+(WINDOW_WIDTH/2)] = gresult.grand2;
 
 // shouldn't it be WINDOW_WIDTH instead of blockDim.x?
+
+	// don't have to do "blockDim.x >> 1" because of faked size of block
+ //   for(unsigned int stride = blockDim.x; stride > 0; stride >>= 1) {
     for(unsigned int stride = blockDim.x >> 1; stride > 0; stride >>= 1) {
         __syncthreads();
         sum_of_trials[tId] += sum_of_trials[tId + stride];
@@ -100,7 +113,7 @@ __global__ void black_scholes_variance_kernel(const double mean,
     
     variances[tId] = cudaTrials[gId];
     variances[tId] = variances[tId] - mean;
-    variances[tId] = variances[tId] *  variances[tId] / (double)M;
+    variances[tId] = (variances[tId] *  variances[tId] )/ (double)M;
 
     for(unsigned int stride = blockDim.x >> 1; stride > 0; stride >>= 1) {
         __syncthreads();
@@ -126,7 +139,16 @@ cit black_scholes(const double S, const double E, const double r,
     assert (trials != NULL);
 
     dim3 dimGrid(num_of_blocks);
+    
+    /* The number of threads in each block was supposed to be WINDOW_WIDTH(128). 
+       However, the feature of gaussian randum number generator forced us to make 
+       work per each thread to be twice of black scholes value operation instead of one. 
+       So, we decide to decrease the ACTUAL number of thread(WINDOW_WIDTH) by the half of
+       , and pretend to operate with WINDOW_WIDTH number of threads in a block. */
+    //dim3 dimBlock(WINDOW_WIDTH/2);
     dim3 dimBlock(WINDOW_WIDTH);
+	////////////////////////////////////////
+	// below pretend working with MOCKED thread number
 
     curandState* randStates;
     cudaMalloc((void **) &randStates, M * sizeof(curandState));
@@ -164,12 +186,16 @@ cit black_scholes(const double S, const double E, const double r,
     
     stddev = black_scholes_stddev (mean, M, cudaTrials);
     cudaMemcpy(trials, cudaTrials, size, cudaMemcpyDeviceToHost);
-    cudaFree(cudaTrials);
-    cudaFree(blockMeans);
+
+	/* confidence interval */
     conf_width = 1.96 * stddev / sqrt ((double) M);
     interval.min = mean - conf_width;
     interval.max = mean + conf_width;
 
+	/* clean up */
+	cudaFree(cudaRands);
+    cudaFree(cudaTrials);
+    cudaFree(blockMeans);
     cudaFree(randStates);
     delete [] trials;
     delete [] means;
@@ -178,21 +204,24 @@ cit black_scholes(const double S, const double E, const double r,
 }
 
 double black_scholes_stddev (const double mean, const long M, double* cudaTrials) {
-    const long numOfBlocks = M/WINDOW_WIDTH;
-    double* variances = new double[numOfBlocks]; 
+    const long num_of_blocks = max((long)1,M/WINDOW_WIDTH);
+    double* variances = new double[num_of_blocks]; 
     double* cudaVariances;
-    cudaMalloc((void**) &cudaVariances, numOfBlocks * sizeof(double));
+    cudaMalloc((void**) &cudaVariances, num_of_blocks * sizeof(double));
     
-    dim3 dimGrid(numOfBlocks);
+    dim3 dimGrid(num_of_blocks);
     dim3 dimBlock(WINDOW_WIDTH);
     black_scholes_variance_kernel<<<dimGrid, dimBlock>>>(mean, M, cudaTrials, cudaVariances);
-    cudaMemcpy(variances, cudaVariances, numOfBlocks * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(variances, cudaVariances, num_of_blocks * sizeof(double), cudaMemcpyDeviceToHost);
     cudaFree(cudaVariances);
     
     double variance = 0.0;
-    for(long idx=0; idx<numOfBlocks; idx++) {
+    for(long idx=0; idx<num_of_blocks; idx++) {
         variance += variances[idx];
     }
+
+	/* clean up */
+	cudaFree(cudaVariances);
     
     return sqrt(variance);
 }
